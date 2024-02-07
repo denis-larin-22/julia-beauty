55ab5c9e8de9881cb0244956fa18f39a596d3cdd
denis-larin-22 <denislarin2017@gmail.com>
2024-02-07 11:28:52 +0200

    Initial app

---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Syne } from 'next/font/google';
+
+export const inter = Inter({
+    subsets: ['latin'],
+    weight: ['400', '700']
+});
+
+export const syne = Syne({
+    subsets: ['latin'],
+    weight: ['600']
+});
