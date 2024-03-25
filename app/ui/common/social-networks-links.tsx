50321106d290cd10f3e0c54b1c34e1128fc7665b
Denis <denislarin2017@gmail.com>
2024-03-25 10:39:25 +0200

    Fixed mistake in the socialNetworksLinks array, title field was missing

--- app/ui/common/social-networks-links.tsx
+++ app/ui/common/social-networks-links.tsx
@@ -3,14 +3,17 @@ import { CallIcon, InstagramIcon, TelegramIcon } from "../assets/links-icons";
 
 export const socialNetworksLinks: IIconObj[] = [
     {
+        title: 'Telegram',
         icon: <TelegramIcon color="black" />,
         href: 'https://t.me/jyliya24'
     },
     {
+        title: 'Call number',
         icon: <CallIcon color="black" />,
         href: 'tel:380667718657'
     },
     {
+        title: 'Instagram',
         icon: <InstagramIcon color="black" />,
         href: 'https://www.instagram.com/julia_magicbeautiful/?igsh=eXo5cGQxdDR5OWZh'
     },
